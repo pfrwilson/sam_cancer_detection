bad9c78f2161be325560150d45a01fa3c418cd75
Paul Wilson <ypfrwilson@gmail.com>
2024-01-19 00:16:54 -0500

    add submission file

--- submission_files/medsam_cancer_detection_v2.sh
+++ submission_files/medsam_cancer_detection_v2.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# Parameters
-#SBATCH --cpus-per-task=16
+#SBATCH --mem=16G
 #SBATCH --gres=gpu:a40:1
-#SBATCH --job-name=submitit
-#SBATCH --mem=16GB
-#SBATCH --nodes=1
-#SBATCH --open-mode=append
-#SBATCH --output=%J.log
-#SBATCH --qos=m3
-#SBATCH --signal=USR2@90
-#SBATCH --time=240
-#SBATCH --wckey=submitit
+#SBATCH --time 8:00:00
+#SBATCH -c 16 
+#SBATCH --qos=m2
+#SBATCH --output=slurm-%j.log
 
-# setup
-module load cuda11.8+cudnn8.9.6
+# send this batch script a SIGUSR1 60 seconds
+# before we hit our time limit
+#SBATCH --signal=B:USR1@60
 
-# command
-export SUBMITIT_EXECUTOR=slurm
-srun --unbuffered --output /fs01/home/pwilson/projects/medAI/projects/sam/logs/medsam_cancer_detection_v3/2023-11-27-11:39:18-inescapable-cuttlefish/fold4/submitit_logs/%j_%t_log.out --error /fs01/home/pwilson/projects/medAI/projects/sam/logs/medsam_cancer_detection_v3/2023-11-27-11:39:18-inescapable-cuttlefish/fold4/submitit_logs/%j_%t_log.err /h/pwilson/anaconda3/envs/ai/bin/python -u -m submitit.core._submit /fs01/home/pwilson/projects/medAI/projects/sam/logs/medsam_cancer_detection_v3/2023-11-27-11:39:18-inescapable-cuttlefish/fold4/submitit_logs
+export TQDM_MININTERVAL=30
+
+python medsam_cancer_detection_v2.py \
+    --augment=v1 \
+    --benign_cancer_ratio_for_training=3 \
+    --name medsam_pca_segmentator_from_segmentation_pretrained_encoder \
+    --image_encoder_checkpoint /h/pwilson/projects/sam_prostate_segmentation/checkpoints/sam_best_nct_dice_0.89_fold0_image_encoder.pth
+
